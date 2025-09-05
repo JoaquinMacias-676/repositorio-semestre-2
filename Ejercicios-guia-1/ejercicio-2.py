c564a4cfbafd0c6fec482b115f0b66185ad25cfd
Joaquin Macias <omermacias@gmail.com>
2025-09-05 03:36:54 -0400

    Subiendo ejercicio 2

---
+++ Ejercicios-guia-1/ejercicio-2.py
@@ -0,0 +1,5 @@
+class FuncionTrigonometrica():
+    def __init__(self, tipo_funcion, amplitud, periodo):
+        self.tipo_funcion = tipo_funcion
+        self.amplitud = amplitud
+        self.periodo = periodo
