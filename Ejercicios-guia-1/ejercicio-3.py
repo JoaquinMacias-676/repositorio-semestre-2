7db2825d6a9812612ce9229c7708c93b4640eb9e
Joaquin Macias <omermacias@gmail.com>
2025-09-09 03:39:02 -0300

    Actualizando ejercicio 3, agregando 2 metodos

--- Ejercicios-guia-1/ejercicio-3.py
+++ Ejercicios-guia-1/ejercicio-3.py
@@ -6,12 +6,12 @@ class Producto():
         self.precio_u = int(precio_u)
         self.stock = int(stock)
         self.codigo_barra = str(codigo_barra)
-        self.historial_stock = []
+        self.historial_stock = [stock]
 
     # Método para actualizar el stock
     def actualizar_stock(self, nuevo_stock):
-        self.historial_stock.append(self.stock) # Agregando el stock anterior al historial
         self.stock = int(nuevo_stock) # Cambiando al nuevo stock
+        self.historial_stock.append(self.stock) # Agregando el stock nuevo al historial
 
     # Método para calcular el valor total del producto
     def valor_total(self):
@@ -20,7 +20,36 @@ class Producto():
     def __str__(self):
         return f"\nProducto: {self.nombre_producto} | Código de Barra: {self.codigo_barra} | Precio Unitario: ${self.precio_u} CLP | Cantidad: {self.stock} | Historial Stock: {self.historial_stock} | Valor Total: ${self.valor_total()} CLP\n"
     
-producto1 = Producto("Cebolla", 500, 20, "7 89200 00129")
+class Inventario():
+    def __init__(self):
+        self.productos = {}
+
+    def agregar_producto(self, producto):
+        if producto.codigo_barra in  self.productos:
+            print(f"El producto, con código de barra {producto.codigo_barra} ya se encuentra en el inventario")
+        else:
+            self.productos[producto.codigo_barra] = producto
+            print(f"Agregando el producto con código de barra {producto.codigo_barra} al Inventario")
+
+    def actualizar_stock_inv(self, codigo_barra, nuevo_stock ):
+        if codigo_barra in self.productos:
+            producto = self.productos[codigo_barra]
+            antiguo_stock = producto.stock
+            producto.actualizar_stock(nuevo_stock)
+            print(f"El producto {producto.nombre_producto} con código de barra {producto} a actualizado su stock de {antiguo_stock} a {producto.stock}.")
+        else:
+            print(f"El producto ingresado no se encuentra en el inventario.")
+
+
+
+producto1 = Producto("Cebolla", 500, 20, "78920000129")
 
 producto1.actualizar_stock(10)
 print(producto1)
+
+inventario1 = Inventario()
+
+inventario1.agregar_producto(producto1)
+inventario1.agregar_producto(producto1)
+
+inventario1.actualizar_stock_inv(78920000129, 40)
