97f945d972efedf523ec76052d005209a2ffec3b
Joaquin Macias <omermacias@gmail.com>
2025-09-11 12:30:31 -0300

    Actualizando lab 1

--- Laboratorios/lab1.py
+++ Laboratorios/lab1.py
@@ -13,11 +13,14 @@ class Gato():
     
     # Creando el método para que el gato juegue
     def jugar(self):
-        energia_perdida = 10
-        hambre_aumentada = 5
-        self.energia -= energia_perdida
-        self.hambre += hambre_aumentada
-        print(f"¡El gato {self.nombre} se puso a jugar!, a perdido {energia_perdida} pts. de energía y a aumentado en {hambre_aumentada} pts. su hambre.") 
+        if self.energia < 20:
+            print(f"{self.nombre} no tiene la energía suficiente para poder jugar")
+        else:
+            energia_perdida = 10
+            hambre_aumentada = 5
+            self.energia -= energia_perdida
+            self.hambre += hambre_aumentada
+            print(f"¡El gato {self.nombre} se puso a jugar!, a perdido {energia_perdida} pts. de energía y a aumentado en {hambre_aumentada} pts. su hambre.") 
 
     # Creando método para alimentar al gato 
     def alimentar(self):
@@ -25,15 +28,14 @@ class Gato():
         hambre_perdida = 15
         self.energia += energia_recuperada
         self.hambre -= hambre_perdida
-        print(f"Haz alimentado a {self.nombre}, por lo que a recuperado {energia_recuperada} pts. de energía, y {hambre_perdida} pts")
+        print(f"Haz alimentado a {self.nombre}, por lo que a recuperado {energia_recuperada} pts. de energía, y a perdido {hambre_perdida} pts. de hambre")
 
     # Creando método mágico para mostrar información del gato
     def __str__(self):
         return f"| Nombre: {self.nombre} | Edad: {self.edad} | Energía: {self.energia} | Hambre {self.hambre} | Color {self.color} | Tamaño(kg): {self.tamanio:.2f} |\n"
         
-    def __delattr__(self, gato):
-        del gato
-        print(f"El Gato {gato.nombre} ha salido del Café")
+    def __del__(self):
+        print(f"El Gato {self.nombre} ha salido del Café")
 
 # Creando la clase "Espacio"
 class Espacio():
@@ -43,17 +45,14 @@ class Espacio():
         self.gatos = []
 
     def agregar_gato(self, gato):
-        if gato in self.gatos:
-            print(f'El gato {gato.nombre}, con ID: {gato.id} ya se encuentra en el espacio "{self.espacio}".')
-        else:
-            nuevo_gato = 1
-            capacidad_nueva = self.capacidad - nuevo_gato # Creando variable para guardar la nueva capacidad
-            self.capacidad = capacidad_nueva
-            if capacidad_nueva < 0:
-                print("La capacidad máxima de gatos se ha excedido, el gato no puede entrar.")
-            elif capacidad_nueva >= 0:
+        if len(self.gatos) < self.capacidad:
+            if gato in self.gatos:
+                print(f'El gato {gato.nombre}, con ID: {gato.id} ya se encuentra en el espacio "{self.espacio}".')
+            else:
                 self.gatos.append(gato)
                 print(f'El gato {gato.nombre} ha sido agregado al espacio "{self.espacio}"')
+        else:
+            print(f"No queda espacio para agregar a {gato.nombre}.")
 
     def __str__(self):
         informacion = "\n==== Gatos ====\n" 
@@ -62,7 +61,7 @@ class Espacio():
         return informacion
 
 # probando a crear una Clase "CafeGatos" para guardar los espacios
-class CafeGatos():
+"""class CafeGatos():
     def __init__(self):
         self.espacios = []
 
@@ -74,11 +73,12 @@ class CafeGatos():
             print(f"El espacio {espacio_nuevo.nombre_espacio} ha sido agregado al Café")
 
     def __str__(self):
-        return f"Espacios: {self.espacios}"
+        return f"Espacios: {self.espacios}" """
 
 # Instanciando atributos del primer gato
 gato1 = Gato("Halfonso", 143, 7, 30, 40, "Naranja", 3)
 gato2 = Gato("Sky", 102, 2, 50, 30, "Gris", 1.5)
+gato3 = Gato("Goku", 78, 3, 60, 40, "Negro", 2)
 
 # Instanciando atributos del espacio
 espacio1 = Espacio("Terraza", 2)
@@ -91,12 +91,14 @@ print(gato1)
 espacio1.agregar_gato(gato1)
 espacio1.agregar_gato(gato1)
 espacio1.agregar_gato(gato2)
+espacio1.agregar_gato(gato3)
 print(espacio1)
 
+"""
 # Instanciando atributo a la Clase "Cafe"
 cafe = CafeGatos()
 
 # Probando Métodos de la Clase "CaféGatos" 
 cafe.agregar_espacio(espacio1)
 
-print(cafe)
+print(cafe) """
