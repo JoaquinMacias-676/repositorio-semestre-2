0c33fb815a0c69314dd84fea1a22cae38765e46c
Joaquin Macias <omermacias@gmail.com>
2025-11-15 20:11:58 -0300

    Moviendo documentos

--- ejemplos/Guía-1-Ejercicios/ejercicio-2.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import math
-
-class FuncionTrigonometrica():
-    def __init__(self, tipo, amplitud= -1, periodo=2*math.pi):
-        self.tipo_funcion = tipo
-        self.amplitud = amplitud
-        self.periodo = periodo
