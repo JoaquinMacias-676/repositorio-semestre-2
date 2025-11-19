1849f75935bf13d868bdde8f61110efcf5257fe5
Joaquin Macias <omermacias@gmail.com>
2025-11-19 12:15:39 -0300

    Importando rectangulos a Main

--- Ejercicios/Ejercicio2-Colisión/Main.py
+++ Ejercicios/Ejercicio2-Colisión/Main.py
@@ -1,5 +1,9 @@
 import pygame as pg
 
+# Importando rectangulos
+from rectangulo_grande import RectanguloGrande
+from rectangulo_pequeño import RectanguloPequeño
+
 pg.init()
 
 window = pg.display.set_mode((800, 600))
