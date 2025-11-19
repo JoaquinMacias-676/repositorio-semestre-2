9a03023d13d4b12858111c6fad01bfd50854e75e
Joaquin Macias <omermacias@gmail.com>
2025-11-19 12:15:27 -0300

    Actualizando rectangulos

--- Ejercicios/Ejercicio2-Colisión/rectangulo_grande.py
+++ Ejercicios/Ejercicio2-Colisión/rectangulo_grande.py
@@ -25,3 +25,16 @@ class RectanguloGrande:
             mov_y = -1
         if teclas[pg.K_s]:
             mov_y = 1
+        
+        self.rect.x += mov_x * self.velocidad
+        self.rect.y += mov_y * self.velocidad
+
+    def restablecer_posicion(self, x, y):
+        self.rect.x = x
+        self.rect.y = y
+
+    def obtener_posicion(self):
+        return (self.rect.x, self.rect.y)
+    
+    def cambiar_color(self, color):
+        self.color = color
